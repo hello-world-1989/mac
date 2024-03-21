f30889b7bd95397b6d366c03b93468328e2ceb88
program think backup <103631137+hello-world-1989@users.noreply.github.com>
2024-03-21 21:04:24 +1300

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,3 @@
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/mac/mac-tor.zip
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/mac/mac-session.zip
+wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/mac/mac-chrome.zip
